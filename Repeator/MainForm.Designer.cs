ab28735126db5b0f2cb04d78acd2b1d7b0b27a9d
amenayach <amenprog@gmail.com>
2016-07-10 23:42:51 +0300

    First hit

--- Repeator/MainForm.Designer.cs
+++ Repeator/MainForm.Designer.cs
@@ -28,20 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tbText = new System.Windows.Forms.RichTextBox();
+            this.btnRepeat = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.numCount = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numCount)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tbText
+            // 
+            this.tbText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbText.BackColor = System.Drawing.Color.Black;
+            this.tbText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbText.Font = new System.Drawing.Font("Comic Sans MS", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbText.ForeColor = System.Drawing.Color.White;
+            this.tbText.Location = new System.Drawing.Point(2, 2);
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(644, 271);
+            this.tbText.TabIndex = 0;
+            this.tbText.Text = "{\n\t\"id\": $i,\n\t\"name\": \"Someone$i\",\n\t\"good\": $b,\n\t\"date\": new Date($i, 1, 1)\n},\\r\\" +
+    "n";
+            this.tbText.TextChanged += new System.EventHandler(this.tbText_TextChanged);
+            this.tbText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbText_KeyDown);
+            // 
+            // btnRepeat
+            // 
+            this.btnRepeat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRepeat.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnRepeat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRepeat.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRepeat.ForeColor = System.Drawing.Color.White;
+            this.btnRepeat.Location = new System.Drawing.Point(521, 289);
+            this.btnRepeat.Name = "btnRepeat";
+            this.btnRepeat.Size = new System.Drawing.Size(111, 37);
+            this.btnRepeat.TabIndex = 1;
+            this.btnRepeat.Text = "&Repeat It!";
+            this.btnRepeat.UseVisualStyleBackColor = true;
+            this.btnRepeat.Click += new System.EventHandler(this.btnRepeat_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.lblCount.ForeColor = System.Drawing.Color.White;
+            this.lblCount.Location = new System.Drawing.Point(262, 296);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(134, 22);
+            this.lblCount.TabIndex = 2;
+            this.lblCount.Text = "&Count to repeat";
+            // 
+            // numCount
+            // 
+            this.numCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.numCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.numCount.Location = new System.Drawing.Point(395, 294);
+            this.numCount.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.numCount.Name = "numCount";
+            this.numCount.Size = new System.Drawing.Size(120, 27);
+            this.numCount.TabIndex = 3;
+            this.numCount.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(644, 338);
+            this.Controls.Add(this.numCount);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnRepeat);
+            this.Controls.Add(this.tbText);
+            this.Icon = global::Repeator.Properties.Resources.Iconsmind_Outline_Code_Window;
+            this.KeyPreview = true;
             this.Name = "MainForm";
             this.Text = "Repeator";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.numCount)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox tbText;
+        private System.Windows.Forms.Button btnRepeat;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.NumericUpDown numCount;
     }
 }
 
--- Repeator/MainForm.cs
+++ Repeator/MainForm.cs
@@ -12,9 +12,63 @@ namespace Repeator
 {
     public partial class MainForm : Form
     {
+
+        Stack<string> undoList = new Stack<string>();
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void btnRepeat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(tbText.Text))
+                {
+
+                    undoList.Push(tbText.Text);
+
+                    var s = tbText.Text;
+                    var result = string.Empty;
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        result += s.Replace("$i", i.ToString())
+                                       .Replace("$b", (i % 2).ToString())
+                                       .Replace("\\r\\n", Environment.NewLine)
+                                       .Replace("\\n", Environment.NewLine);
+                    }
+
+                    tbText.Text = result;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void tbText_TextChanged(object sender, EventArgs e)
+        {
+            undoList.Push(tbText.Text);
+        }
+
+        private void tbText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Z && (e.Control))
+            {
+                tbText.Text = undoList.Pop();
+            }
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Alt && e.KeyCode == Keys.C)
+            {
+                numCount.Focus();
+            }
         }
     }
 }
